7eefe337d305009763a87a311a2fa2e4ee1b1dd4
Z <a@b.c>
2023-06-11 01:33:32 -0600

    Fixed a bug where gpu memory was not being released on new model switch.

--- webui.py
+++ webui.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,jsonify
 from wtforms import Form, TextAreaField, FileField, SelectField, IntegerField, FloatField, SubmitField
 from wtforms.validators import DataRequired, NumberRange
 from scipy.io import wavfile
@@ -55,7 +55,11 @@ def predict(model, text, melody, sr, duration, topk, topp, temperature, cfg_coef
     global MODEL
     topk = int(topk)
     if MODEL is None or MODEL.name != model:
+        if MODEL is not None:
+            del MODEL
+            torch.cuda.empty_cache()
         MODEL = load_model(model)
+        print(MODEL)
         return predict(model, text, melody, sr, duration, topk, topp, temperature, cfg_coef)
 
     #if duration > MODEL.lm.cfg.dataset.segment_duration:
@@ -84,13 +88,6 @@ def predict(model, text, melody, sr, duration, topk, topp, temperature, cfg_coef
     output = output.detach().cpu().numpy()
     return MODEL.sample_rate, output
 
-def mp3_to_wav(mp3_path, wav_path):
-    # Load the MP3 file with librosa
-    y, sr = librosa.load(mp3_path, sr=None)  # sr=None preserves the original sampling rate
-
-    # Save the data as a WAV file with soundfile
-    sf.write(wav_path, y, sr, format='wav')
-
 class MusicForm(Form):
     text = TextAreaField('Input Text', [DataRequired()])
     melody = FileField('Optional Melody')
@@ -104,18 +101,17 @@ class MusicForm(Form):
 
 app = Flask(__name__)
 
+@app.route('/api/audio-files', methods=['GET'])
+def get_audio_files():
+    audio_files = [(f, f, os.path.getmtime(f'static/audio/{f}')) for f in os.listdir('static/audio')]
+    audio_files_dicts = [{'text': text, 'audio_file': audio_file, 'timestamp': timestamp} for text, audio_file, timestamp in audio_files]
+    return jsonify(audio_files_dicts)
+
 @app.route('/', methods=['GET', 'POST'])
-def home():
+def home_and_submit():
     form = MusicForm(request.form)
 
-    if not os.path.exists('static/audio'):
-        os.makedirs('static/audio')
-        
-    audio_files = [(f, f, os.path.getmtime(f'static/audio/{f}')) for f in os.listdir('static/audio')]
-    #audio_files.sort(key=itemgetter(2), reverse=True)  # sort by timestamp in descending order
-
     if request.method == 'POST' and form.validate():
-        # Use the form data to call the predict function
         model = form.model.data
         text = form.text.data
         duration = form.duration.data
@@ -123,41 +119,35 @@ def home():
         topp = form.topp.data
         temperature = form.temperature.data
         cfg_coef = form.cfg_coef.data
-        
-        print(
-            f"model = {form.model.data}\n"
-            f"text = {form.text.data}\n"
-            f"duration = {form.duration.data}\n"
-            f"topk = {form.topk.data}\n"
-            f"topp = {form.topp.data}\n"
-            f"temperature = {form.temperature.data}\n"
-            f"cfg_coef = {form.cfg_coef.data}"
-        )
 
         melody = None
         sr = None
         if 'melody' in request.files and request.files['melody'].filename != '':
-            melody_file = request.files['melody']
-            extension = os.path.splitext(melody_file.filename)[1]
-            if extension.lower() in ['.wav', '.mp3']:
-                melody, sr = librosa.load(melody_file, sr=None)  # librosa automatically handles both wav and mp3 files including different wav encodings
+            if form.model.data != 'melody':
+                pass
             else:
-                print(f"Unsupported file extension: {extension}")
+                melody_file = request.files['melody']
+                extension = os.path.splitext(melody_file.filename)[1]
+                if extension.lower() in ['.wav', '.mp3']:
+                    melody, sr = librosa.load(melody_file, sr=None)
+                else:
+                    print(f"Unsupported file extension: {extension}")
 
         output = predict(model, text, melody, sr, duration, topk, topp, temperature, cfg_coef)
-
-
         output_filename = save_output(output, form.text.data)
-
-        # Remove the 'static/audio/' prefix from the filename for display
-        display_filename = output_filename.rsplit('/', 1)[-1]
         
-        audio_files.append((display_filename, display_filename, os.path.getmtime(output_filename)))
-        audio_files.sort(key=itemgetter(2), reverse=True)  # sort by timestamp in descending order
+    if not os.path.exists('static/audio'):
+        os.makedirs('static/audio')
 
-    return render_template('form.html', form=form, audio_files=audio_files)
+    audio_files = [(f, f, os.path.getmtime(f'static/audio/{f}')) for f in os.listdir('static/audio')]
+    audio_files_dicts = [{'text': text, 'audio_file': audio_file, 'timestamp': timestamp} for text, audio_file, timestamp in audio_files]
+    
+    if request.method == 'POST':
+        return jsonify({'audio_files': audio_files_dicts})
+    else:
+        return render_template('form.html', form=form, audio_files=audio_files_dicts)
 
 if __name__ == '__main__':
     if not os.path.exists('static/audio'):
         os.makedirs('static/audio')
-    app.run(debug=True)
+    app.run()
